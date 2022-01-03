b4452c58223ebda2b3d14faa54eb446c4429bdcd
blakos1 <blakos1@st.amu.edu.pl>
2022-01-03 16:27:53 +0100

    adds excel file saving

--- webscraper.R
+++ webscraper.R
@@ -1,5 +1,6 @@
 library(RSelenium)
 library(wdman)
+library(stringr)
 
 # requirements:
 #   java with PATH set up
@@ -22,6 +23,8 @@ firefox_location = "C:/Users/U576750/AppData/Local/Mozilla Firefox/firefox.exe"
 rD = rsDriver(browser = "firefox",
               verbose = FALSE,
               extraCapabilities = list(
+                acceptInsecureCerts = TRUE,
+                acceptUntrustedCerts = TRUE,
                 "moz:firefoxOptions" = list(
                   binary = firefox_location
                 )
@@ -43,6 +46,141 @@ pwbox$sendKeysToElement(list(password))
 logxpath = "//button[contains(text(), 'Login')]"
 logbox = remDr$findElement(using = "xpath", value = logxpath)
 logbox$clickElement()
+
+rm(unbox, pwbox, logxpath, logbox, username, password, pwtable)
+Sys.sleep(3)
+
+#----
+#if acc is locked go to unlock page, else print that acc is unlocked
+# Sys.sleep(5)
+# lockimg = remDr$findElement(using = "id", "ctl00_MenuControl_Img_Login")
+# 
+# if(exists("lockimg")){
+#   if(lockimg$isElementDisplayed()[[1]] == TRUE){
+#     print("Account is locked")
+#     remDr$navigate("https://www.trademap.org/stCaptcha.aspx")
+#     # captchabox = remDr$findElement(using = "class", "div_captchaImg")
+#   } else {
+#     print("Lock image not found")
+#   }
+# } else {
+#   print("Account is not locked")
+# }
+
+
+# captchaxpath = "/html/body/form/div[3]/table/tbody/tr/td/div/div[2]/div/img"
+# captcha = remDr$findElement(using = "xpath", value = captchaxpath)
+# captchasrc = captcha$getElementAttribute("src")[[1]]
+# remDr$navigate(captchasrc)
+
+
+# removedids = c("header", "footer", "marmenu", "ctl00_TitleContent", "ctl00_PageContent_Label1")
+# 
+# for (i in (1:length(removedids))){
+#   remscript = paste("return document.getElementById(\'removedids[i]').remove();",
+#                  sep = "")
+#   remscript = str_replace(remscript, fixed("removedids[i]"), removedids[i])
+#   remDr$executeScript(remscript)
+# }
+# 
+# remDr$setWindowSize(300, 300)
+# remDr$screenshot(display = TRUE)
+# remDr$maxWindowSize()
+# 
+# captcha = readline(prompt = "Captcha content: ")
+# 
+# textbox = remDr$findElement(using = "id", "ctl00_PageContent_CaptchaAnswer")
+# textbox$sendKeysToElement(list(captcha))
+# 
+# valxpath = '//*[@id="ctl00_PageContent_ButtonvalidateCaptcha"]'
+# validatebtn = remDr$findElement(using = "xpath", valxpath)
+# validatebtn$clickElement()
+# 
+# itc_homepage = "https://www.trademap.org/Index.aspx"
+# remDr$navigate(itc_homepage)
+
+# countrylist = c("Poland", "Germany")
+# countrybox_id = "ctl00_PageContent_RadComboBox_Country_Input"
+# countrybox = remDr$findElement(using = "id", countrybox_id)
+# countrybox$clickElement()
+# countrybox$sendKeysToElement(list(countrylist[1]))
+# 
+# countrybox_id2 = "ctl00_PageContent_RadComboBox_Country_c0"
+# countrybox2 = remDr$findElement(using = "id", countrybox_id2)
+# countrybox2$clickElement()
+
+# m_bttn = "ctl00_PageContent_Button_TimeSeries_M"
+# monthly_bttn = remDr$findElement(using = "id", m_bttn)
+# monthly_bttn$clickElement()
+
+
+# 
+# remDr$mouseMoveToLocation(webElement = logg)
+# remDr$click()
+
+# validatebtn$click(buttonId = 1)
+# valpos = validatebtn$getElementLocation()
+# remDr$mouseMoveToLocation(valpos$x, valpos$y)
+# remDr$refresh()
+# remDr$getCurrentUrl()
+
+
+
+#link generator ----
+
+country_code = "203" # 616 Poland
+partner_code = "" #203 Czech Republic
+ntl_code2 = c("70109067")
+
+exports = 2
+imports = 1
+export_import = c(exports, imports)
+
+code_list = c("10","21","31","41","43",
+              "45","47","51","53","55",
+              "57","61","67","71","79",
+              "91","99"
+              )
+
+# for (i in code_list){
+#   ntl_code = paste0("701090", i)
+# 
+#   for (i in export_import){
+#     
+#     
+#     
+#     
+#     
+#   }
+# }
+
+
+link = paste0(
+"https://www.trademap.org/Country_SelCountry_MQ_TS.aspx?nvpm=1%7c",
+country_code,
+"%7c%7c",
+partner_code,
+"%7c%7c",
+ntl_code,
+"%7c%7c%7c",
+nchar(ntl_code),
+"%7c1%7c1%7c",
+exports,
+"%7c2%7c3%7c2%7c2%7c1%7c1"
+)
+
+remDr$navigate("https://www.trademap.org/Country_SelCountry_MQ_TS.aspx?nvpm=1%7c%7c%7c%7c%7cTOTAL%7c%7c%7c2%7c1%7c1%7c2%7c2%7c3%7c2%7c1%7c1%7c1")
+Sys.sleep(2)
+remDr$navigate(link)
+
+Sys.sleep(2)
+saveexcel_id = "ctl00_PageContent_GridViewPanelControl_ImageButton_ExportExcel"
+saveexcel = remDr$findElement(using = "id", saveexcel_id)
+saveexcel$clickElement()
+
+
+
+
 
 
 #shutdown --------------------
